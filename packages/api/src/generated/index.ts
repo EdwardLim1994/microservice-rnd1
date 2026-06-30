38ddfc2c288f8b49b218808372a05272e79f96bc
EdwardLim1994 <edwardlimkoksiong1994@gmail.com>
2026-06-30 20:28:13 +0800

    chore: :fire: Remove Agent skills

--- packages/api/src/generated/index.ts
+++ packages/api/src/generated/index.ts
@@ -1,11 +1,11 @@
 // auto-generated, do not edit
-export * as Demo1Graphql from './demo1/graphql';
-export * as Demo1Demo1Proto from './demo1/proto/demo1';
-export * as Demo1TyperegistryProto from './demo1/proto/typeRegistry';
-export * as Demo1GoogleProtobuf from './demo1/proto/google/protobuf';
-export * as Demo1Google from './demo1/proto/google';
-export * as Demo2Graphql from './demo2/graphql';
-export * as Demo2Demo2Proto from './demo2/proto/demo2';
-export * as Demo2TyperegistryProto from './demo2/proto/typeRegistry';
-export * as Demo2GoogleProtobuf from './demo2/proto/google/protobuf';
-export * as Demo2Google from './demo2/proto/google';
+export * as Demo2Graphql from "./demo2/graphql"
+export * as Demo2Demo2Proto from "./demo2/proto/demo2"
+export * as Demo2TyperegistryProto from "./demo2/proto/typeRegistry"
+export * as Demo2GoogleProtobuf from "./demo2/proto/google/protobuf"
+export * as Demo2Google from "./demo2/proto/google"
+export * as Demo1Graphql from "./demo1/graphql"
+export * as Demo1Demo1Proto from "./demo1/proto/demo1"
+export * as Demo1TyperegistryProto from "./demo1/proto/typeRegistry"
+export * as Demo1GoogleProtobuf from "./demo1/proto/google/protobuf"
+export * as Demo1Google from "./demo1/proto/google"
