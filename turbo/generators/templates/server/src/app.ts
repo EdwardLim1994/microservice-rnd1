c466e0e9f36d4cba4d718794e71f2aac72bd07ea
Edward Lim Kok Siong <edwardlimkoksiong1994@gmail.com>
2026-07-06 10:44:33 +0800

    fix: :bug: Return empty driver when generating new server

--- turbo/generators/templates/server/src/app.ts
+++ turbo/generators/templates/server/src/app.ts
@@ -1,5 +1,5 @@
 import { CronDriver, ServerApp } from "server";
 
 export default async function main() {
-	await ServerApp.init(CronDriver).run(() => `Server is running`);
+	await ServerApp.init([]).run(() => `Server is running`);
 }
