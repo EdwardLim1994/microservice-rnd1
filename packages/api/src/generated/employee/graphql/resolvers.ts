d43a8d1eddd6c61f2597ff210383a31da831a173
TapiruWanderCode94 <tapiruwandercode94@gmail.com>
2026-07-21 09:23:58 +0800

    chore: apply biome check --write to regenerated api output

--- packages/api/src/generated/employee/graphql/resolvers.ts
+++ packages/api/src/generated/employee/graphql/resolvers.ts
@@ -2,15 +2,17 @@ import type { GraphQLResolveInfo } from 'graphql';
 import type { EmployeeContextType } from './context';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & {
+  [P in K]-?: NonNullable<T[P]>;
+};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: { input: string; output: string; }
-  String: { input: string; output: string; }
-  Boolean: { input: boolean; output: boolean; }
-  Int: { input: number; output: number; }
-  Float: { input: number; output: number; }
-  _FieldSet: { input: unknown; output: unknown; }
+  ID: { input: string; output: string };
+  String: { input: string; output: string };
+  Boolean: { input: boolean; output: boolean };
+  Int: { input: number; output: number };
+  Float: { input: number; output: number };
+  _FieldSet: { input: unknown; output: unknown };
 };
 
 /** An employee registered in the HR system, owned by the employee subgraph. */
@@ -51,7 +53,6 @@ export type Mutation = {
    */
   registerEmployee: RegisterEmployeeResult;
 };
-
 
 export type MutationRegisterEmployeeArgs = {
   input: RegisterEmployeeInput;
@@ -94,46 +95,73 @@ export type ResolversObject<TObject> = WithIndex<TObject>;
 export type ResolverTypeWrapper<T> = Promise<T> | T;
 
 export type ReferenceResolver<TResult, TReference, TContext> = (
-      reference: TReference,
-      context: TContext,
-      info: GraphQLResolveInfo
-    ) => Promise<TResult> | TResult;
+  reference: TReference,
+  context: TContext,
+  info: GraphQLResolveInfo,
+) => Promise<TResult> | TResult;
 
-      type ScalarCheck<T, S> = S extends true ? T : NullableCheck<T, S>;
-      type NullableCheck<T, S> = Maybe<T> extends T ? Maybe<ListCheck<NonNullable<T>, S>> : ListCheck<T, S>;
-      type ListCheck<T, S> = T extends (infer U)[] ? NullableCheck<U, S>[] : GraphQLRecursivePick<T, S>;
-      export type GraphQLRecursivePick<T, S> = { [K in keyof T & keyof S]: ScalarCheck<T[K], S[K]> };
-    
+type ScalarCheck<T, S> = S extends true ? T : NullableCheck<T, S>;
+type NullableCheck<T, S> =
+  Maybe<T> extends T ? Maybe<ListCheck<NonNullable<T>, S>> : ListCheck<T, S>;
+type ListCheck<T, S> = T extends (infer U)[]
+  ? NullableCheck<U, S>[]
+  : GraphQLRecursivePick<T, S>;
+export type GraphQLRecursivePick<T, S> = {
+  [K in keyof T & keyof S]: ScalarCheck<T[K], S[K]>;
+};
 
 export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
   resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
 };
-export type Resolver<TResult, TParent = Record<PropertyKey, never>, TContext = Record<PropertyKey, never>, TArgs = Record<PropertyKey, never>> = ResolverFn<TResult, TParent, TContext, TArgs> | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
+export type Resolver<
+  TResult,
+  TParent = Record<PropertyKey, never>,
+  TContext = Record<PropertyKey, never>,
+  TArgs = Record<PropertyKey, never>,
+> =
+  | ResolverFn<TResult, TParent, TContext, TArgs>
+  | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
 
 export type ResolverFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
   args: TArgs,
   context: TContext,
-  info: GraphQLResolveInfo
+  info: GraphQLResolveInfo,
 ) => Promise<TResult> | TResult;
 
 export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
   args: TArgs,
   context: TContext,
-  info: GraphQLResolveInfo
+  info: GraphQLResolveInfo,
 ) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
 
 export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
   args: TArgs,
   context: TContext,
-  info: GraphQLResolveInfo
+  info: GraphQLResolveInfo,
 ) => TResult | Promise<TResult>;
 
-export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
-  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
-  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+export interface SubscriptionSubscriberObject<
+  TResult,
+  TKey extends string,
+  TParent,
+  TContext,
+  TArgs,
+> {
+  subscribe: SubscriptionSubscribeFn<
+    { [key in TKey]: TResult },
+    TParent,
+    TContext,
+    TArgs
+  >;
+  resolve?: SubscriptionResolveFn<
+    TResult,
+    { [key in TKey]: TResult },
+    TContext,
+    TArgs
+  >;
 }
 
 export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
@@ -141,30 +169,60 @@ export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
   resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
 }
 
-export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+export type SubscriptionObject<
+  TResult,
+  TKey extends string,
+  TParent,
+  TContext,
+  TArgs,
+> =
   | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
   | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
 
-export type SubscriptionResolver<TResult, TKey extends string, TParent = Record<PropertyKey, never>, TContext = Record<PropertyKey, never>, TArgs = Record<PropertyKey, never>> =
-  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+export type SubscriptionResolver<
+  TResult,
+  TKey extends string,
+  TParent = Record<PropertyKey, never>,
+  TContext = Record<PropertyKey, never>,
+  TArgs = Record<PropertyKey, never>,
+> =
+  | ((
+      ...args: any[]
+    ) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
   | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
 
-export type TypeResolveFn<TTypes, TParent = Record<PropertyKey, never>, TContext = Record<PropertyKey, never>> = (
+export type TypeResolveFn<
+  TTypes,
+  TParent = Record<PropertyKey, never>,
+  TContext = Record<PropertyKey, never>,
+> = (
   parent: TParent,
   context: TContext,
-  info: GraphQLResolveInfo
+  info: GraphQLResolveInfo,
 ) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
 
-export type IsTypeOfResolverFn<T = Record<PropertyKey, never>, TContext = Record<PropertyKey, never>> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+export type IsTypeOfResolverFn<
+  T = Record<PropertyKey, never>,
+  TContext = Record<PropertyKey, never>,
+> = (
+  obj: T,
+  context: TContext,
+  info: GraphQLResolveInfo,
+) => boolean | Promise<boolean>;
 
 export type NextResolverFn<T> = () => Promise<T>;
 
-export type DirectiveResolverFn<TResult = Record<PropertyKey, never>, TParent = Record<PropertyKey, never>, TContext = Record<PropertyKey, never>, TArgs = Record<PropertyKey, never>> = (
+export type DirectiveResolverFn<
+  TResult = Record<PropertyKey, never>,
+  TParent = Record<PropertyKey, never>,
+  TContext = Record<PropertyKey, never>,
+  TArgs = Record<PropertyKey, never>,
+> = (
   next: NextResolverFn<TResult>,
   parent: TParent,
   args: TArgs,
   context: TContext,
-  info: GraphQLResolveInfo
+  info: GraphQLResolveInfo,
 ) => TResult | Promise<TResult>;
 
 /** Mapping of federation types */
@@ -174,12 +232,11 @@ export type FederationTypes = ResolversObject<{
 
 /** Mapping of federation reference types */
 export type FederationReferenceTypes = ResolversObject<{
-  Employee:
-    ( { __typename: 'Employee' }
-    & GraphQLRecursivePick<FederationTypes['Employee'], {"id":true}> );
+  Employee: { __typename: 'Employee' } & GraphQLRecursivePick<
+    FederationTypes['Employee'],
+    { id: true }
+  >;
 }>;
-
-
 
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = ResolversObject<{
@@ -207,8 +264,18 @@ export type ResolversParentTypes = ResolversObject<{
   Boolean: Scalars['Boolean']['output'];
 }>;
 
-export type EmployeeResolvers<ContextType = EmployeeContextType, ParentType extends ResolversParentTypes['Employee'] = ResolversParentTypes['Employee'], FederationReferenceType extends FederationReferenceTypes['Employee'] = FederationReferenceTypes['Employee']> = ResolversObject<{
-  __resolveReference?: ReferenceResolver<Maybe<ResolversTypes['Employee']> | FederationReferenceType, FederationReferenceType, ContextType>;
+export type EmployeeResolvers<
+  ContextType = EmployeeContextType,
+  ParentType extends
+    ResolversParentTypes['Employee'] = ResolversParentTypes['Employee'],
+  FederationReferenceType extends
+    FederationReferenceTypes['Employee'] = FederationReferenceTypes['Employee'],
+> = ResolversObject<{
+  __resolveReference?: ReferenceResolver<
+    Maybe<ResolversTypes['Employee']> | FederationReferenceType,
+    FederationReferenceType,
+    ContextType
+  >;
   createdAt?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   firstName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
@@ -217,22 +284,51 @@ export type EmployeeResolvers<ContextType = EmployeeContextType, ParentType exte
   id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
   lastName?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
   salaryPerDay?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
-  supervisor?: Resolver<Maybe<ResolversTypes['Employee']>, ParentType, ContextType>;
+  supervisor?: Resolver<
+    Maybe<ResolversTypes['Employee']>,
+    ParentType,
+    ContextType
+  >;
   supervisorId?: Resolver<Maybe<ResolversTypes['ID']>, ParentType, ContextType>;
   updatedAt?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
 }>;
 
-export type MutationResolvers<ContextType = EmployeeContextType, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = ResolversObject<{
-  registerEmployee?: Resolver<ResolversTypes['RegisterEmployeeResult'], ParentType, ContextType, RequireFields<MutationRegisterEmployeeArgs, 'input'>>;
+export type MutationResolvers<
+  ContextType = EmployeeContextType,
+  ParentType extends
+    ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation'],
+> = ResolversObject<{
+  registerEmployee?: Resolver<
+    ResolversTypes['RegisterEmployeeResult'],
+    ParentType,
+    ContextType,
+    RequireFields<MutationRegisterEmployeeArgs, 'input'>
+  >;
 }>;
 
-export type QueryResolvers<ContextType = EmployeeContextType, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = ResolversObject<{
-  employees?: Resolver<Array<ResolversTypes['Employee']>, ParentType, ContextType>;
+export type QueryResolvers<
+  ContextType = EmployeeContextType,
+  ParentType extends
+    ResolversParentTypes['Query'] = ResolversParentTypes['Query'],
+> = ResolversObject<{
+  employees?: Resolver<
+    Array<ResolversTypes['Employee']>,
+    ParentType,
+    ContextType
+  >;
 }>;
 
-export type RegisterEmployeeResultResolvers<ContextType = EmployeeContextType, ParentType extends ResolversParentTypes['RegisterEmployeeResult'] = ResolversParentTypes['RegisterEmployeeResult']> = ResolversObject<{
+export type RegisterEmployeeResultResolvers<
+  ContextType = EmployeeContextType,
+  ParentType extends
+    ResolversParentTypes['RegisterEmployeeResult'] = ResolversParentTypes['RegisterEmployeeResult'],
+> = ResolversObject<{
   employee?: Resolver<ResolversTypes['Employee'], ParentType, ContextType>;
-  temporaryPassword?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  temporaryPassword?: Resolver<
+    ResolversTypes['String'],
+    ParentType,
+    ContextType
+  >;
 }>;
 
 export type Resolvers<ContextType = EmployeeContextType> = ResolversObject<{
@@ -241,4 +337,3 @@ export type Resolvers<ContextType = EmployeeContextType> = ResolversObject<{
   Query?: QueryResolvers<ContextType>;
   RegisterEmployeeResult?: RegisterEmployeeResultResolvers<ContextType>;
 }>;
-
